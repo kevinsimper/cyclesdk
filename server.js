65e6bb78a18b2546edb223cdd09a579621868d29
Kevin Simper <kevin.simper@gmail.com>
2016-02-25 00:31:46 +0100

    Hot reload the server with restarting it

--- server.js
+++ server.js
@@ -6,5 +6,17 @@ app.use(function(req, res, next) {
   require('./dist/server').default(req, res, next)
 })
 
+var chokidar = require('chokidar')
+var watcher = chokidar.watch('./dist')
+
+watcher.on('ready', function() {
+  watcher.on('all', function() {
+    console.log("Clearing /dist/ module cache from server")
+    Object.keys(require.cache).forEach(function(id) {
+      if (/[\/\\]dist[\/\\]/.test(id)) delete require.cache[id];
+    });
+  });
+});
+
 const PORT = 9000
 app.listen(PORT, () => console.log('Listening on', PORT))
