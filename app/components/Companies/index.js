3e27ad8f7c3ab4d131b0db34091fa115a98ccfd7
Kevin Simper <kevin.simper@gmail.com>
2016-02-26 18:04:13 +0100

    Make a button that is a link

--- app/components/Companies/index.js
+++ app/components/Companies/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import RaisedButton from 'material-ui/lib/raised-button'
+import styles from './style.scss'
 
 export default class Companies extends Component {
   render() {
-    console.log(this.props.companies)
     return (
-      <table>
+      <table className={styles.Table}>
         <thead>
           <tr>
             <th>Name</th>
@@ -17,6 +18,9 @@ export default class Companies extends Component {
               <tr>
                 <td>{company.name}</td>
                 <td><a href={company.url}>{company.url}</a></td>
+                <td>
+                  <RaisedButton label="Besøg" primary={true} linkButton={true} href={company.url}/>
+                </td>
               </tr>
             )
           })}
