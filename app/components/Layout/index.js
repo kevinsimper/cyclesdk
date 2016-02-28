6841958ef721a0aeda53dd9f9bacbfd0be519371
Kevin Simper <kevin.simper@gmail.com>
2016-02-29 00:49:04 +0100

    Add analytics to site

--- app/components/Layout/index.js
+++ app/components/Layout/index.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react'
 import styles from './style.scss'
 
 export default class Layout extends Component {
+  getAnalytics() {
+    return {
+      __html: `
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-4309863-21', 'auto')
+        ga('send', 'pageview')
+      `
+    }
+  }
   render() {
     return (
       <html>
         <head>
           <title>Cycles.dk</title>
           <link rel='stylesheet' href='/styles.css'/>
+          <script dangerouslySetInnerHTML={this.getAnalytics()}></script>
         </head>
         <body>
           <div className={styles.wrapper}>
