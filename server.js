4e27d3815b38badcef6bc81755941318bd16b07d
Kevin Simper <kevin.simper@gmail.com>
2016-02-29 15:20:00 +0100

    Only use webpack in development

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ let express = require('express')
 let webpack = require('webpack')
 let app = express()
 
-let config = require('./webpack.client.config')
-let compiler = webpack(config)
+if(process.env.NODE_ENV !== 'production') {
+  let config = require('./webpack.client.config')
+  let compiler = webpack(config)
 
-app.use(require("webpack-dev-middleware")(compiler, {
-  noInfo: true, publicPath: config.output.publicPath
-}))
-app.use(require("webpack-hot-middleware")(compiler))
+  app.use(require("webpack-dev-middleware")(compiler, {
+    noInfo: true, publicPath: config.output.publicPath
+  }))
+  app.use(require("webpack-hot-middleware")(compiler))
+}
+
 
 app.use(function(req, res, next) {
   require('./dist/server').default(req, res, next)
