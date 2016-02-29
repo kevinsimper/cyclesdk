a9ed3d7c11b7af0fa7100a965d04486dc6cffedc
Kevin Simper <kevin.simper@gmail.com>
2016-02-29 14:19:19 +0100

    ignore-file-loader

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -42,8 +42,8 @@ module.exports = {
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
-            'file?hash=sha512&digest=hex&name=[hash].[ext]',
-            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          'ignore-file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
         ]
       },
       { test: /\.md$/, loader: "html!markdown" }
