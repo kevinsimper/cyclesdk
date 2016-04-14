894942afe2b1f0324a9448782d921564f7d1aae2
Kevin Simper <kevin.simper@gmail.com>
2016-04-13 19:50:58 -0500

    Add wrongly moved stylesheet link

--- app/components/Layout/index.js
+++ app/components/Layout/index.js
@@ -19,6 +19,7 @@ export default class Layout extends Component {
       <html>
         <head>
           <title>Cycles.dk</title>
+          <link rel='stylesheet' href={this.props.assets.main.css}/>
           <script dangerouslySetInnerHTML={this.getAnalytics()}></script>
         </head>
         <body>
