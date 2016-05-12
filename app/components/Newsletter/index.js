b2f68fe77c7f8de31db27bc48764543e65550e7c
Kevin Simper <kevin.simper@gmail.com>
2016-05-12 13:10:38 -0500

    Submit the email to the api with axios

--- app/components/Newsletter/index.js
+++ app/components/Newsletter/index.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react'
 import styles from './style.scss'
+import axios from 'axios'
 
 export default class Newsletter extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      email: ''
+    }
+  }
+  onChangeName(e) {
+    this.setState({
+      name: e.target.value
+    })
+  }
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+  onClickSubmit() {
+    axios.post('/api/subscriber', {
+      name: this.state.name,
+      email: this.state.email
+    })
+  }
   render() {
     return (
       <div className={styles.Form}>
         <h1>Tilmeld nyhedsbrev og få gode tilbud på cykelrejser!</h1>
         <div>
           <label>Name</label>
-          <input type="text"/>
+          <input type="text" onChange={this.onChangeName.bind(this)} value={this.state.name}/>
         </div>
         <div>
           <label>Email</label>
-          <input type="text"/>
+          <input type="text" onChange={this.onChangeEmail.bind(this)} value={this.state.email}/>
         </div>
         <div>
-          <button>Tilmeld</button>
+          <button onClick={this.onClickSubmit.bind(this)}>Tilmeld</button>
         </div>
       </div>
     )
