89d116e0a0bdcd0cf27478e2549f4ac7addd2b03
Kevin Simper <kevin.simper@gmail.com>
2016-05-14 22:15:54 -0500

    Add logo to header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import styles from './style.scss'
 import Menu from '../Menu'
+import logo from './logo.svg'
 
 export default class Header extends Component {
   render() {
     return (
       <div>
         <div className={styles.Header}>
-          <h1 className={styles.Title}>Cycles.dk</h1>
+          <div className={styles.LogoContainer}>
+            <img src={logo} className={styles.Logo} />
+            <h1 className={styles.Title}>Cycles.dk</h1>
+          </div>
           <div className={styles.Spacer}>-</div>
           <h2 className={styles.SubTitle}>Find din næste cykelrejse</h2>
         </div>
