8f029906a6eaf35e6a0be6f85a1cc6baf24ee54b
Kevin Simper <kevin.simper@gmail.com>
2016-06-05 16:07:34 -0500

    Make logo clickable

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -7,14 +7,14 @@ export default class Header extends Component {
   render() {
     return (
       <div>
-        <div className={styles.Header}>
+        <a href={'/'} className={styles.Header}>
           <div className={styles.LogoContainer}>
             <img src={logo} className={styles.Logo} />
             <h1 className={styles.Title}>Cycles.dk</h1>
           </div>
           <div className={styles.Spacer}>-</div>
           <h2 className={styles.SubTitle}>Find din næste cykelrejse</h2>
-        </div>
+        </a>
         <Menu/>
       </div>
     )
