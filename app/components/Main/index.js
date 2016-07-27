392a9b6de59e9b43803987b8a9be9fdd61dd920a
Kevin Simper <kevin.simper@gmail.com>
2016-07-27 10:42:59 +0200

    Have a wider layout on admin

--- app/components/Main/index.js
+++ app/components/Main/index.js
@@ -4,16 +4,21 @@ import Content from '../Content'
 import styles from './style.scss'
 import Helmet from 'react-helmet'
 import Footer from '../Footer'
+import cx from 'classnames'
 
 export default class Main extends Component {
   render() {
+    let ContentClasses = cx({
+      [styles.Content]: true,
+      [styles.Admin]: this.props.routes[1].path === 'admin'
+    })
     return (
       <div className={styles.wrapper}>
         <Helmet
           defaultTitle={'Alt omkring cykelrejser - Cycles.dk'}
         />
         <Header/>
-        <div className={styles.Content}>
+        <div className={ContentClasses}>
           <Content>
           {this.props.children}
           </Content>
